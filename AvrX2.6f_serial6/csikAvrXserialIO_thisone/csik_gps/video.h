12295c89ef70a11da8f425d6a92f3a7c2d3b2793
csik <csik@f291d19c-9823-0410-bfda-c3a9b5f085b8>
2007-05-17 00:14:40 +0000

    More clean up.

--- AvrX2.6f_serial6/csikAvrXserialIO_thisone/csik_gps/video.h
+++
@@ -1,6 +0,0 @@
-/**********************************************************************************************/
-/*	video.h																					  */
-/**********************************************************************************************/
-
-void initVideo(void);
-void writeVideoScreen(float LAT, float LONG, float TIME, float ALTITUDE, float COMPASS, float SPEED, char ERROR);
